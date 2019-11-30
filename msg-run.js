7047414fadc1be006746f40526c8142314cd8466
rjrodger <richard@ricebridge.com>
2019-11-30 14:26:14 +0000

    v0.1.1

--- msg-run.js
+++ msg-run.js
@@ -1,12 +1,10 @@
 /* Copyright (c) 2019 voxgig and other contributors, MIT License */
 'use strict'
 
-
 const Util = require('util')
 
 const Joi = require('@hapi/joi')
 const XState = require('xstate')
-
 
 module.exports = msg_run
 module.exports.defaults = {
@@ -14,22 +12,25 @@ module.exports.defaults = {
   clock: Joi.object(),
   spec: Joi.object({
     interval: Joi.number().default(111),
-    tests: Joi.array().items({
-      name: Joi.string().required(),
-      scenario: Joi.array(),
-    }).required()
-  }).default().required()
+    tests: Joi.array()
+      .items({
+        name: Joi.string().required(),
+        scenario: Joi.array()
+      })
+      .required()
+  })
+    .default()
+    .required()
 }
 module.exports.errors = {}
-
 
 function msg_run(options) {
   var seneca = this
 
   const clock = intern.make_clock(options)
-  
+
   const machine = intern.make_scenario_machine(options.spec, intern.validate)
-  
+
   const pi = {
     spec: options.spec,
     previous: {},
@@ -42,10 +43,9 @@ function msg_run(options) {
     },
 
     // TODO: save as entity
-    store: [],
+    store: []
   }
-  
-  
+
   seneca
     .message('sys:msg-run,cmd:start', cmd_start)
     .message('sys:msg-run,cmd:stop', cmd_stop)
@@ -54,7 +54,7 @@ function msg_run(options) {
     .message('sys:msg-run,get:status', get_status)
     .message('sys:msg-run,get:history', get_history)
 
-    // .prepare(async function prepare_msg_run() {})
+  // .prepare(async function prepare_msg_run() {})
 
   async function cmd_start(msg) {
     var run_seneca = this.root.delegate()
@@ -75,7 +75,7 @@ function msg_run(options) {
 
   async function get_status(msg) {
     var current_test = pi.status.current_test
-    if(current_test.start) {
+    if (current_test.start) {
       current_test.elapsed = Date.now() - current_test.start
     }
     return this.util.deep(pi.status)
@@ -84,33 +84,36 @@ function msg_run(options) {
   async function get_history(msg) {
     var history = {
       // TODO: move get_status to intern pure function
-      status: await get_status.call(this,msg)
+      status: await get_status.call(this, msg)
     }
-    
-    if(msg.full) {
+
+    if (msg.full) {
       history.store = pi.store
     }
 
     // summary table
     else {
       var table = []
-      for(var i = pi.store.length-1; pi.store.length-10 < i && -1 < i; --i) {
+      for (
+        var i = pi.store.length - 1;
+        pi.store.length - 10 < i && -1 < i;
+        --i
+      ) {
         var run = pi.store[i]
         var passed = 0
         var failed = 0
         var fail_names = []
-        
-        for(var tI = 0; tI < run.tests.length; tI++) {
+
+        for (var tI = 0; tI < run.tests.length; tI++) {
           var test = run.tests[tI]
-          if(test.pass) {
+          if (test.pass) {
             passed++
-          }
-          else {
+          } else {
             failed++
             fail_names.push(test.name)
           }
         }
-        
+
         var row = [
           run.start,
           run.run,
@@ -129,7 +132,6 @@ function msg_run(options) {
 
     return history
   }
-
 }
 
 const intern = (msg_run.intern = {
@@ -139,57 +141,58 @@ const intern = (msg_run.intern = {
     var match = scenario_step.out
     var actual = ctx.res.out
 
-    var check = ctx.seneca().util.Optioner(match, {must_match_literals: true})
+    var check = ctx.seneca().util.Optioner(match, { must_match_literals: true })
     var result = check(actual)
-    
+
     //var pass = JSON.stringify(match) === JSON.stringify(actual)
-    
+
     return null == result.error
   },
 
   execute_test: async function(seneca, test_spec, machine) {
-    return new Promise(resolve=>{
-
+    return new Promise(resolve => {
       var machine_context = {
         seneca: () => seneca,
-        finish: (ctx) => resolve(ctx), 
+        finish: ctx => resolve(ctx),
         test_spec: test_spec,
         index: 0,
-        results: [],
+        results: []
       }
 
-      const interpreter =
-            intern.make_scenario_interpreter(machine, machine_context)
-    
+      const interpreter = intern.make_scenario_interpreter(
+        machine,
+        machine_context
+      )
+
       interpreter.start()
     })
   },
 
   runner: function(seneca, clock, msg, pi, machine) {
-    if(pi.status.running) return pi.status;
-    
+    if (pi.status.running) return pi.status
+
     pi.status.running = true
     run_tests()
 
-    return pi.status;
-    
+    return pi.status
+
     async function run_tests() {
-      if(!pi.status.running) return;
+      if (!pi.status.running) return
 
       pi.previous = pi.current
 
       pi.current = {
         start: Date.now(),
-        tests: [],
+        tests: []
       }
-      
+
       var tests = pi.spec.tests
-      
-      for(var tI = 0; tI < tests.length; tI++) {
-        if(!pi.status.running) {
+
+      for (var tI = 0; tI < tests.length; tI++) {
+        if (!pi.status.running) {
           break
         }
-        
+
         var test_spec = tests[tI]
         var test_start = Date.now()
 
@@ -198,11 +201,11 @@ const intern = (msg_run.intern = {
 
         var test_result = await intern.execute_test(seneca, test_spec, machine)
         // console.log('TR', test_result)
-        
+
         pi.status.current_test.name = null
         pi.status.current_test.start = null
         pi.status.current_test.elapsed = null
-        
+
         test_result.name = test_spec.name
         test_result.start = test_start
         test_result.end = Date.now()
@@ -219,8 +222,8 @@ const intern = (msg_run.intern = {
       intern.store(pi)
 
       pi.status.runs++
-      
-      if(pi.status.running) {
+
+      if (pi.status.running) {
         clock.setTimeout(run_tests, pi.spec.interval)
       }
     }
@@ -229,12 +232,12 @@ const intern = (msg_run.intern = {
     return intern.runner(seneca, clock, msg, pi, machine)
   },
 
-  stop: function(msg,pi) {
+  stop: function(msg, pi) {
     pi.status.running = false
     return pi.status
   },
-  
-  get_test_results: function (msg, src) {
+
+  get_test_results: function(msg, src) {
     var include_log = !!msg.log
 
     var out = {
@@ -242,11 +245,11 @@ const intern = (msg_run.intern = {
       start: src.start,
       end: src.end,
       duration: src.duration,
-      tests: src.tests,
+      tests: src.tests
     }
 
     // include msg and response data
-    if(include_log) {
+    if (include_log) {
       out.log = src.log
     }
 
@@ -254,18 +257,17 @@ const intern = (msg_run.intern = {
   },
 
   store: function(pi) {
-    if(pi.current.start) {
-      var stored_test = {...pi.current}
+    if (pi.current.start) {
+      var stored_test = { ...pi.current }
       pi.store.push(stored_test)
     }
   },
-
 
   make_scenario_machine: function(spec, validate) {
     const config = {
       actions: {
         update_res: XState.assign({
-          res: (ctx, event) => ({kind:'out', msg:ctx.msg, out:event.data})
+          res: (ctx, event) => ({ kind: 'out', msg: ctx.msg, out: event.data })
         }),
 
         validate_res: XState.assign({
@@ -273,14 +275,14 @@ const intern = (msg_run.intern = {
             return validate(ctx)
           }
         }),
-        
+
         result_entry: (ctx, event) => {
           ctx.results.push({
             msg: ctx.msg,
-            res: ctx.res,
+            res: ctx.res
           })
           ctx.index++
-        },
+        }
       },
       services: {
         outbound_send: (ctx, event) => {
@@ -290,86 +292,83 @@ const intern = (msg_run.intern = {
           var msgparts = ctx.test_spec.scenario[index].msg
           msgparts = Array.isArray(msgparts) ? msgparts : [msgparts]
           msgparts.unshift({})
-          msgparts = msgparts.map(x=>ctx.seneca().util.Jsonic(x))
+          msgparts = msgparts.map(x => ctx.seneca().util.Jsonic(x))
 
-          ctx.msg = ctx.seneca().util.deep.apply(null,msgparts)
+          ctx.msg = ctx.seneca().util.deep.apply(null, msgparts)
           return ctx.seneca().post(ctx.msg)
         }
       },
       guards: {
-        result_is_invalid: (ctx) => {
+        result_is_invalid: ctx => {
           return !ctx.valid
         },
-        result_has_more_msgs: (ctx) => {
-          return ctx.index < (ctx.test_spec.scenario.length-1)
+        result_has_more_msgs: ctx => {
+          return ctx.index < ctx.test_spec.scenario.length - 1
         }
       }
     }
-    
-    const machine = XState.Machine({
-      id: 'msgrun',
-      initial: 'outbound',
-      states: {
 
-        outbound: {
-          invoke: {
-            id: 'send',
-            src: 'outbound_send',
-            onDone: {
-              target: 'result',
-              actions: [
-                'update_res',
-                'validate_res',
-              ]
-            },
-            onError: {
-              target: 'result'
+    const machine = XState.Machine(
+      {
+        id: 'msgrun',
+        initial: 'outbound',
+        states: {
+          outbound: {
+            invoke: {
+              id: 'send',
+              src: 'outbound_send',
+              onDone: {
+                target: 'result',
+                actions: ['update_res', 'validate_res']
+              },
+              onError: {
+                target: 'result'
+              }
             }
-          }
-        },
-
-        inbound: {
-          on: {
-            MESSAGE: 'message',
-            RESULT: 'result',
-          }
-        },
-
-        message: {
-          on: { '': 'inbound' }
-        },
-
-        result: {
-          entry: 'result_entry',
-          on: {
-            '': [
-              { target: 'fail', cond: 'result_is_invalid' },
-              { target: 'outbound', cond: 'result_has_more_msgs' },
-              { target: 'pass' }
-            ]
-          }
-        },
-        
-        pass: {
-          entry: XState.assign({pass:true}),
-          on: { '': 'stop' }
-        },
-        
-        fail: {
-          entry: XState.assign({pass:false}),
-          on: { '': 'stop' }
-        },
-
-        stop: {
-          entry: (ctx) => {
-            ctx.finish(ctx)
           },
-          type: 'final',
-        }
-        
-      }
-    }, config)
 
+          inbound: {
+            on: {
+              MESSAGE: 'message',
+              RESULT: 'result'
+            }
+          },
+
+          message: {
+            on: { '': 'inbound' }
+          },
+
+          result: {
+            entry: 'result_entry',
+            on: {
+              '': [
+                { target: 'fail', cond: 'result_is_invalid' },
+                { target: 'outbound', cond: 'result_has_more_msgs' },
+                { target: 'pass' }
+              ]
+            }
+          },
+
+          pass: {
+            entry: XState.assign({ pass: true }),
+            on: { '': 'stop' }
+          },
+
+          fail: {
+            entry: XState.assign({ pass: false }),
+            on: { '': 'stop' }
+          },
+
+          stop: {
+            entry: ctx => {
+              ctx.finish(ctx)
+            },
+            type: 'final'
+          }
+        }
+      },
+      config
+    )
 
     return machine
   },
@@ -378,10 +377,9 @@ const intern = (msg_run.intern = {
     ctx.mark = Math.random()
     const machine_instance = machine.withContext(ctx)
 
-    const interpreter = XState
-          .interpret(machine_instance)
-          .onTransition(state => {
-            /*
+    const interpreter = XState.interpret(machine_instance).onTransition(
+      state => {
+        /*
             console.log(intern.aligner([
               0, 'TRANSITION',
               12, state.value,
@@ -392,45 +390,43 @@ const intern = (msg_run.intern = {
               0, state.context.index
             ]))
             */
-          })
-    
+      }
+    )
+
     return interpreter
   },
 
   make_clock: function(options) {
     var clock = options.clock
 
-    if(!options.clock) {
+    if (!options.clock) {
       clock = {
         setTimeout: setTimeout,
         setInterval: setInterval,
         clearTimeout: clearTimeout,
-        clearInterval: clearInterval,
+        clearInterval: clearInterval
       }
     }
 
     return clock
   },
 
-  aligner: function (line) {
+  aligner: function(line) {
     var out = []
-    for(var i = 0; i < line.length; i+=2) {
+    for (var i = 0; i < line.length; i += 2) {
       var len = line[i]
-      var str = line[i+1]
-      
-      if('string' !== typeof(str)) {
-        str = Util.inspect(str, {breakLength: Infinity})
+      var str = line[i + 1]
+
+      if ('string' !== typeof str) {
+        str = Util.inspect(str, { breakLength: Infinity })
       }
-      
-      if(0 == len) {
+
+      if (0 == len) {
         len = str.length + 1
       }
-      
+
       out.push(str.padEnd(len, ' '))
     }
     return out.join('')
   }
-  
 })
-
-
--- test/msg-run.test.js
+++ test/msg-run.test.js
@@ -16,86 +16,84 @@ const Plugin = require('..')
 
 lab.test('validate', PluginValidator(Plugin, module))
 
-lab.test('happy', {timeout:3333}, async () => {
+lab.test('happy', { timeout: 3333 }, async () => {
   var spec = {
     interval: 100,
     tests: [
       {
         name: 't0',
-        scenario: [
-          { msg:['a:1', {x:2}], out: {x:2} },
-        ]
+        scenario: [{ msg: ['a:1', { x: 2 }], out: { x: 2 } }]
       },
       {
         name: 't1',
         scenario: [
-          { msg:['a:1', {x:3}], out: {x:3} },
-          { msg:['a:2', {y:4}], out: {y:4} },
-          { msg:['a:1', {x:5}], out: {x:5} },
+          { msg: ['a:1', { x: 3 }], out: { x: 3 } },
+          { msg: ['a:2', { y: 4 }], out: { y: 4 } },
+          { msg: ['a:1', { x: 5 }], out: { x: 5 } }
         ]
-      },
+      }
     ]
   }
 
   var clock = Lolex.createClock()
-  
-  var si = await seneca_instance({},{spec:spec,clock:clock}).ready()
+
+  var si = await seneca_instance({}, { spec: spec, clock: clock }).ready()
   expect(si).exists()
 
-
-  si.message('a:1', async (m) => {
-    return {x: m.x}
+  si.message('a:1', async m => {
+    return { x: m.x }
   })
 
-  si.message('a:2', async (m) => {
-    return {y: m.y}
+  si.message('a:2', async m => {
+    return { y: m.y }
   })
 
-
-  
   var status0 = await si.post('sys:msg-run,get:status')
   //console.log('status0', status0)
   expect(status0).contains({ running: false, runs: 0 })
-  
+
   var current0 = await si.post('sys:msg-run,get:current')
   //console.log('current0', current0)
-  expect(current0).equals({ start: undefined,
-                            end: undefined,
-                            run: undefined,
-                            duration: undefined,
-                            tests: undefined })
-  
+  expect(current0).equals({
+    start: undefined,
+    end: undefined,
+    run: undefined,
+    duration: undefined,
+    tests: undefined
+  })
+
   var previous0 = await si.post('sys:msg-run,get:previous')
   //console.log('previous0', previous0)
-  expect(previous0).equals({ start: undefined,
-                             end: undefined,
-                             run: undefined,
-                             duration: undefined,
-                             tests: undefined })
+  expect(previous0).equals({
+    start: undefined,
+    end: undefined,
+    run: undefined,
+    duration: undefined,
+    tests: undefined
+  })
 
-
-  
   var start0 = await si.post('sys:msg-run,cmd:start')
   //console.log('start0', start0)
-  expect(start0).contains({running:true})
-  
+  expect(start0).contains({ running: true })
+
   // runs all the msgs of all the tests to completion
   await si.ready()
   //await sleep(55)
-  
+
   var status1 = await si.post('sys:msg-run,get:status')
   //console.log('status1', status1)
   expect(status1).contains({ running: true, runs: 1 })
 
   var previous1 = await si.post('sys:msg-run,get:previous')
   //console.log('previous1', previous1)
-  expect(previous1).equals({ start: undefined,
-                             end: undefined,
-                             run: undefined,
-                             duration: undefined,
-                             tests: undefined })
+  expect(previous1).equals({
+    start: undefined,
+    end: undefined,
+    run: undefined,
+    duration: undefined,
+    tests: undefined
+  })
 
-  
   var current1 = await si.post('sys:msg-run,get:current')
   //console.log('current1')
   //console.dir(current1,{depth:5})
@@ -104,7 +102,6 @@ lab.test('happy', {timeout:3333}, async () => {
   expect(current1.tests.length).equal(2)
   expect(current1.tests[0].results.length).equal(1)
   expect(current1.tests[1].results.length).equal(3)
-
 
   var history0 = await si.post('sys:msg-run,get:history,full:true')
   //console.log('HISTORY')
@@ -119,13 +116,11 @@ lab.test('happy', {timeout:3333}, async () => {
   clock.tick(100)
   await si.ready()
 
-
   var history2 = await si.post('sys:msg-run,get:history')
   //console.log('HISTORY SUMMARY')
   //console.dir(history2)
   expect(history2.summary.length).equal(2)
 
-  
   var status2 = await si.post('sys:msg-run,get:status')
   //console.log('status2', status2)
   expect(status2).contains({ running: true, runs: 2 })
@@ -133,7 +128,6 @@ lab.test('happy', {timeout:3333}, async () => {
   var stop0 = await si.post('sys:msg-run,cmd:stop')
   //console.log('stop0', stop0)
   expect(stop0).contains({ running: false, runs: 2 })
-  
 
   clock.tick(100)
   await si.ready()
@@ -142,14 +136,12 @@ lab.test('happy', {timeout:3333}, async () => {
   //console.log('status3', status3)
   expect(status3).contains({ running: false, runs: 2 })
 
-
   clock.tick(100)
   await si.ready()
 
   var status4 = await si.post('sys:msg-run,get:status')
   //console.log('status4', status4)
   expect(status4).contains({ running: false, runs: 2 })
-
 
   var start1 = await si.post('sys:msg-run,cmd:start')
   //console.log('start1', start1)
@@ -162,7 +154,6 @@ lab.test('happy', {timeout:3333}, async () => {
   //console.log('status5', status5)
   expect(status5).contains({ running: true, runs: 3 })
 
-
   var current2 = await si.post('sys:msg-run,get:current')
   //console.log('current2')
   //console.dir(current2,{depth:5})
@@ -172,7 +163,6 @@ lab.test('happy', {timeout:3333}, async () => {
   expect(current2.tests[0].results.length).equal(1)
   expect(current2.tests[1].results.length).equal(3)
 
-
   var previous2 = await si.post('sys:msg-run,get:previous')
   expect(previous2.run).equal(1)
   expect(previous2.duration).above(0)
@@ -180,43 +170,39 @@ lab.test('happy', {timeout:3333}, async () => {
   expect(previous2.tests[0].results.length).equal(1)
   expect(previous2.tests[1].results.length).equal(3)
 
-
   var history3 = await si.post('sys:msg-run,get:history')
   //console.log('HISTORY')
   //console.dir(history3)
-  expect(history3.summary.length).equal(3)  
+  expect(history3.summary.length).equal(3)
 })
 
-
-lab.test('validate', {timeout:3333}, async () => {
+lab.test('validate', { timeout: 3333 }, async () => {
   var spec = {
     interval: 100,
     tests: [
       {
         name: 't0',
         scenario: [
-          { msg:['a:1', {x:2}], out: {x:2} },
-          { msg:['a:1', {x:3}], out: {x:3} },
-          { msg:['a:1', {x:4}], out: {x:4} },
+          { msg: ['a:1', { x: 2 }], out: { x: 2 } },
+          { msg: ['a:1', { x: 3 }], out: { x: 3 } },
+          { msg: ['a:1', { x: 4 }], out: { x: 4 } }
         ]
-      },
+      }
     ]
   }
 
   var clock = Lolex.createClock()
-  
-  var si = await seneca_instance({},{spec:spec,clock:clock}).ready()
+
+  var si = await seneca_instance({}, { spec: spec, clock: clock }).ready()
   expect(si).exists()
 
-
-  si.message('a:1', async (m) => {
-    return 3 === m.x ? {x: 'bad'} : {x: m.x}
+  si.message('a:1', async m => {
+    return 3 === m.x ? { x: 'bad' } : { x: m.x }
   })
-
 
   await si.post('sys:msg-run,cmd:start')
   await si.ready()
-  
+
   var status0 = await si.post('sys:msg-run,get:status')
   //console.log('status0', status0)
   expect(status0).contains({ running: true, runs: 1 })
@@ -230,19 +216,15 @@ lab.test('validate', {timeout:3333}, async () => {
   //console.log('HISTORY')
   //console.dir(history0,{depth:6})
   expect(history0.summary.length).equal(1)
-  
 
   clock.tick(100)
   await si.ready()
-
 
   var history1 = await si.post('sys:msg-run,get:history')
   //console.log('HISTORY')
   //console.dir(history1,{depth:6})
   expect(history1.summary.length).equal(2)
-  
 })
-
 
 /*
 async function sleep(time) {
